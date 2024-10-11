304d8247d6f773141a2ed422ef9e42f16773077f
basechase <chase7y@gmail.com>
2024-10-11 10:15:58 -0500

    updated vector2

--- MathLibrary/Vector2.cs
+++ MathLibrary/Vector2.cs
@@ -6,7 +6,67 @@ using System.Threading.Tasks;
 
 namespace MathLibrary
 {
-    internal class Vector2
+    public class Vector2
     {
+        float x, y;
+
+        public Vector2()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        //op overload for addition
+
+       public static Vector2 operator +(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.x + right.x, left.y + right.y);
+        }
+
+        //op overload for subtraction
+
+        public static Vector2 operator -(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.x - right.x, left.y - right.y);
+        }
+
+
+        //op overload for multipication by a vector
+        public static Vector2 operator *(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.x * right.x, left.y * right.y);
+        }
+
+        //op overload for multplication by a scalar
+        public static Vector2 operator *(Vector2 left, float right)
+        {
+            return new Vector2(left.x * right, left.y * right);
+        }
+
+        //op overload for division
+        public static Vector2 operator /(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.x / right.x, left.y / right.y);
+        }
+
+        //implicit conversion from System.Numerics.Vector2 to Vector2
+        public static implicit operator Vector2(System.Numerics.Vector2 vector)
+        {
+            return new Vector2(vector.X, vector.Y);
+        }
+
+        //implicit conversion from Vector2 to System.Numerics.Vector2
+        public static implicit operator System.Numerics.Vector2(Vector2 vector)
+        {
+            return new System.Numerics.Vector2(vector.x, vector.y);
+        }
+
+
     }
 }
