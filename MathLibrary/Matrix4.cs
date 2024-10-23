7e3f01f8116117421341175d7d0c77adcf0cfed5
basechase <chase7y@gmail.com>
2024-10-23 11:53:08 -0500

    added matrix4
    need to add all matrix4 functions

---
+++ MathLibrary/Matrix4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+
+    public struct Matrix4
+    {
+        public float
+            m00, m01, m02, m03,
+            m10, m11, m12, m13,
+            m20, m21, m22, m23;
+
+
+        public Matrix4(
+            float m00, float m01, float m02, float m03,
+            float m10, float m11, float m12, float m13,
+            float m20, float m21, float m22, float m23)
+        {
+            this.m00 = m00;
+            this.m01 = m01;
+            this.m02 = m02;
+            this.m03 = m03;
+            this.m10 = m10;
+            this.m11 = m11;
+            this.m12 = m12;
+            this.m13 = m13;
+            this.m20 = m20;
+            this.m21 = m21;
+            this.m22 = m22;
+            this.m23 = m23;
+
+        }
+
+
+    }
+}
